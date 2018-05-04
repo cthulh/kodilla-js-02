60f5846120e175883fe490e6f8a8addc55901b69
Mariusz Borowiak <borowiakm85@gmail.com>
2018-05-04 09:52:54 +0100

    Vars and main function layout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 (function(){
-  console.log("loaded!");
+
+  var buttonRock = document.getElementById('rock');
+  var buttonPaper = document.getElementById('paper');
+  var buttonScissors = document.getElementById('scissors');
+  var outcome = document.getElementById('outcome');
+
+  function playerMove(move){
+    // takes players move
+    // generates random number 1 - 3
+    // associate each number with computers move
+    // choose outcome from array of keys of player moves vs computer moves
+    // return outcome
+
+    // write outcome in a div
+  }
+
 }());
