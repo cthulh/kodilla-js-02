502e52f53af2cf9fe53dfbb5106bc164318903f2
Mariusz Borowiak <borowiakm85@gmail.com>
2018-05-06 20:04:58 +0100

    Add new game modal functions

--- js/script.js
+++ js/script.js
@@ -19,6 +19,17 @@
     element.innerHTML = content;
   }
 
+  // check if is a number
+  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+  // start a new game
+  function resetGame(rounds){
+    var roundsBox = document.getElementById("roundsToWin");
+    writeTo(roundsBox, ("Rounds to win: " + rounds));
+  }
+
   function playerMove(move){
     // takes players move
     // generates random number 1 - 3
@@ -126,6 +137,43 @@
     // write outcome in a div
     updateOutcome(outcome);
     updateResults(result);
+  }
+
+  // Get the modal
+  var modal = document.getElementById('newGameModal');
+
+  // Get the button that opens the modal
+  var newGameBtn = document.getElementById("new_game");
+
+  // Get the <span> element that closes the modal
+  var submitBtn = document.getElementsByClassName("submit")[0];
+
+  // Get the modal message p
+  var modalMessage = document.getElementById("modalMessage");
+
+  // When the user clicks the button, open the modal
+  newGameBtn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  // When the user clicks on submit button hide modal
+  submitBtn.onclick = function() {
+    var rounds = document.getElementById("rounds");
+    if (isNumber(rounds.value)) {
+      resetGame(rounds.value);
+      modal.style.display = "none";
+      rounds.value = "";
+    } else {
+      modalMessage.innerHTML = "Please enter a numeric value!";
+      rounds.value = "";
+    }
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
   }
 
 
